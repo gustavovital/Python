5f31cab93af955e84a77a6c7039f029c8a7fcd48
Gustavo de Oliveira <gustavovital@id.uff.br>
2021-03-17 00:25:02 +0000

    add python for finance

---
+++ Python Programming for Finance/Intro and Getting Stock Price Data - Python Programming for Finance p.1.py
@@ -0,0 +1,25 @@
+"""
+Intro and Getting Stock Price Data - Python Programming for Finance p.1
+Author: PythonProgramming
+Date: 17/03/2021
+"""
+
+# Modules and definitions
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
+
+style.use('ggplot')
+
+# Begining of the script
+start = dt.datetime(2015, 1, 1)
+end = dt.datetime.now()
+
+# Dataframe
+df = web.DataReader('TSLA', 'yahoo', start, end)
+print(df.head())
+
+plt.plot(df['Close'])
+
---
+++ Support Vector Machine with Scikit Learn/Our Method and where we will be getting our Data.py
@@ -0,0 +1,7 @@
+"""
+Our Method and where we will be getting our Data
+Author: pythonprogramming
+Date: 14/03/2021
+
+Our Method and where we will be getting our Data - module 2
+"""
