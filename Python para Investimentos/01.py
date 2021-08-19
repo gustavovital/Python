d10c583068b577adf087bf2992281bf7be950be8
Gustavo de Oliveira <gustavovital@id.uff.br>
2021-08-19 04:27:34 +0100

    update 01

---
+++ Python para Investimentos/01.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 19 00:16:54 2021
+
+@author: gusta
+"""
+
+# modulos necessarios
+import yfinance as yf
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import pandas_datareader as web 
+
+yf.pdr_override()
+
+# obtendo os dados
+ibov = web.get_data_yahoo("^BVSP")
+ibov.head()
